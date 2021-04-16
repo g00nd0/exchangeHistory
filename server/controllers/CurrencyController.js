7fc82b4239121295628b4b3729737d40ee9c3f86
g00nd0 <mitchellgoon@hotmail.com>
2021-04-16 15:49:26 +0800

    Add status codes for axios calls

--- server/controllers/CurrencyController.js
+++ server/controllers/CurrencyController.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const methodOverride = require("method-override");
 const axios = require("axios").default;
+const { StatusCodes } = require("http-status-codes");
 router.use(methodOverride("_method"));
 
 const api_key = process.env.API_KEY;
@@ -13,7 +14,14 @@ router.get("/", async (req, res) => {
   await axios
     .get(`${url}latest?base=SGD&api_key=${api_key}`)
     .then((response) => {
-      res.send(response.data);
+      if (response) {
+        res.status(StatusCodes.OK).send(response.data);
+      } else {
+        res.status(StatusCodes.BAD_REQUEST).send("Unknown Error");
+      }
+    })
+    .catch((error) => {
+      res.status(StatusCodes.BAD_REQUEST).send(error);
     });
 });
 
@@ -30,18 +38,29 @@ router.get("/:reqCurr", async (req, res) => {
     .then(
       axios.spread((...responses) => {
         // const reqCurrency = req.params.reqCurr
+
         const currencyVal = responses[0].data.response;
         const currencyInfo =
           responses[1].data.response.fiats[req.params.reqCurr.toUpperCase()];
 
-        res.send({ val: currencyVal, info: currencyInfo });
+        if (currencyInfo) {
+          res
+            .status(StatusCodes.OK)
+            .send({ val: currencyVal, info: currencyInfo });
+        } else {
+          res.status(StatusCodes.NOT_FOUND).send("Error, no such currency");
+        }
       })
-    );
+    )
+    .catch((error) => {
+      res.status(StatusCodes.BAD_REQUEST).send(error);
+    });
 });
 
 router.get("/:reqCurr/history", async (req, res) => {
+  // ideally, this implementation should use the "timeseries" parameter specified in currencyscoop's documentation, but this requires a paid subscription
   //get hisotry of specified currency (only for three days for now)
-  const timePeriod = 14; // cant display for last 3 days, seems like its because its too soon and data is unavailable, so choose between last 7 and last 14 days
+  const timePeriod = 7; // cant display for last 3 days depending on requested currency, seems like its because its too soon and data is unavailable
 
   let axiosList = new Array();
 
@@ -77,15 +96,20 @@ router.get("/:reqCurr/history", async (req, res) => {
     axiosList.push(axiosCmd(date(i)));
   }
 
-  await axios.all(axiosList).then(
-    axios.spread((...responses) => {
-      res.send(
-        responses.map((oneCurrency) => {
-          return oneCurrency.data.response;
-        })
-      );
-    })
-  );
+  await axios
+    .all(axiosList)
+    .then(
+      axios.spread((...responses) => {
+        res.status(StatusCodes.OK).send(
+          responses.map((oneCurrency) => {
+            return oneCurrency.data.response;
+          })
+        );
+      })
+    )
+    .catch((error) => {
+      res.status(StatusCodes.BAD_REQUEST).send(error);
+    });
 });
 
 module.exports = router;
