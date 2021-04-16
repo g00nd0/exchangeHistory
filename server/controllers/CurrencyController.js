c97c0d7cba0545f0668348059a3595d4d8577cea
g00nd0 <mitchellgoon@hotmail.com>
2021-04-16 13:01:03 +0800

    Add params for foreign currency in URL request

--- server/controllers/CurrencyController.js
+++ server/controllers/CurrencyController.js
@@ -1,18 +1,56 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const methodOverride = require("method-override");
 const axios = require("axios").default;
 router.use(methodOverride("_method"));
 
+const api_key = process.env.API_KEY;
+const url = "https://api.currencyscoop.com/v1/";
+
 router.get("/", async (req, res) => {
   // get all currencies
   await axios
+    .get(`${url}latest?base=SGD&api_key=${api_key}`)
+    .then((response) => {
+      res.send(response.data);
+    });
+});
+
+router.get("/:reqCurr", async (req, res) => {
+  //get one currency
+  await axios
     .get(
-      "https://api.currencyscoop.com/v1/latest?base=SGD&api_key=27530082513705f7bdf5e8478bbf873c"
+      `${url}latest?base=SGD&symbols=${req.params.reqCurr}&api_key=${api_key}`
     )
     .then((response) => {
       res.send(response.data);
     });
+});
+
+router.get("/:reqCurr/history", async (req, res) => {
+  //get hisotry of specified currency (only for three days for now)
+  await axios
+    .all([
+      axios.get(
+        `${url}historical?base=SGD&symbols=${req.params.reqCurr}&date=2020-04-15&api_key=${api_key}`
+      ),
+      axios.get(
+        `${url}historical?base=SGD&symbols=${req.params.reqCurr}&date=2020-04-16&api_key=${api_key}`
+      ),
+      axios.get(
+        `${url}historical?base=SGD&symbols=${req.params.reqCurr}&date=2020-04-17&api_key=${api_key}`
+      ),
+    ])
+    .then(
+      axios.spread((...responses) => {
+        res.send(
+          responses.map((oneCurrency) => {
+            return oneCurrency.data.response;
+          })
+        );
+      })
+    );
 });
 
 module.exports = router;
