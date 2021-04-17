6bf7427d9ac9d49e47f22fb6d3947717abb25ec0
g00nd0 <mitchellgoon@hotmail.com>
2021-04-18 00:01:52 +0800

    Remove unused navbar items, add props

--- client/src/component/NavBar/NavBar.js
+++ client/src/component/NavBar/NavBar.js
@@ -13,28 +13,23 @@ import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Current-See</Navbar.Brand>
+      <Navbar.Brand href="/">Current-See</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Currencies</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="/">Currencies</Nav.Link>
         </Nav>
-        <div className="mr-sm-2">Base</div>
-        <SearchBar></SearchBar>
+        <div className="mr-sm-2">Base Currency</div>
+        <SearchBar
+          currencyInfo={props.currencyInfo}
+          baseCurr={props.baseCurr}
+          setBaseCurr={props.setBaseCurr}
+        />
+
+        {console.log(props.currencyInfo)}
       </Navbar.Collapse>
     </Navbar>
   );
